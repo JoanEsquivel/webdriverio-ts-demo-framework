2e1491774827b89c56edcd0c42f5a06e8b05f57c
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-01-16 19:05:41 -0800

    Update locators.ts

--- test/specs/locators.ts
+++ test/specs/locators.ts
@@ -1,101 +1,109 @@
 describe('Locators - Test Scenarios',async ()=>{
-    it('CSS example', async () => {
+
+    beforeEach(async ()=>{
         await browser.url('https://demoqa.com/links')
-
-        const createdLink = await $('#created')
-
-        await createdLink.click()
-
-        await expect($('#linkResponse')).toHaveTextContaining('Link has responded with staus 201 and status text Created')
         
+    })
+    it('CSS example', async () => {
+       
+        const createdLink = await $('#created')
+        const linkResponse = await $('#linkResponse')
 
+        createdLink.scrollIntoView()
+        createdLink.click()
+        linkResponse.scrollIntoView()
+        await expect(linkResponse.getText()).toHaveTextContaining('Link has responded with staus 201 and status text Created')
+        
     })
 
     it('CSS by index example(nth-child)', async () => {
-        await browser.url('https://demoqa.com/links')
 
         const unauthorizedLink = await $('div#linkWrapper p:nth-child(9) a')
+        const linkResponse = await $('#linkResponse')
 
-        await unauthorizedLink.click()
-
-        await expect($('#linkResponse')).toHaveTextContaining('Link has responded with staus 401 and status text Unauthorized')
+        unauthorizedLink.scrollIntoView()
+        unauthorizedLink.click()
+        linkResponse.scrollIntoView()
+        await expect(linkResponse.getText()).toHaveTextContaining('Link has responded with staus 401 and status text Unauthorized')
     })
 
     it('Link Text(=) example', async () => {
 
         //To get an anchor element with a specific text in it, query the text starting with an equals (=) sign.
-        await browser.url('https://demoqa.com/links')
 
         const noContentLink = await $('=No Content')
+        const linkResponse = await $('#linkResponse')
 
-        await noContentLink.click()
-
-        await expect($('#linkResponse')).toHaveTextContaining('Link has responded with staus 204 and status text No Content')
+        noContentLink.scrollIntoView()
+        noContentLink.click()
+        linkResponse.scrollIntoView()
+        await expect(linkResponse.getText()).toHaveTextContaining('Link has responded with staus 204 and status text No Content')
     })
 
     it('Partial Link Text(*=) example', async () => {
 
         //To find a anchor element whose visible text partially matches your search value, query it by using *= in front of the query string (e.g. *=driver).
-
-        await browser.url('https://demoqa.com/links')
-
         const badRequestLink = await $('*=Request')
+        const linkResponse = await $('#linkResponse')
 
-        await badRequestLink.click()
+        badRequestLink.scrollIntoView()
+        badRequestLink.click()
 
-        await expect($('#linkResponse')).toHaveTextContaining('Link has responded with staus 400 and status text Bad Request')
+        linkResponse.scrollIntoView()
+        await expect(linkResponse.getText()).toHaveTextContaining('Link has responded with staus 400 and status text Bad Request')
     })
 
     it('Element with certain text(<element>={text}) example', async () => {
 
         //To find a anchor element whose visible text partially matches your search value, query it by using *= in front of the query string (e.g. *=driver).
-
-        await browser.url('https://demoqa.com/links')
-
         const forbiddenLink = await $('a=Forbidden')
+        const linkResponse = await $('#linkResponse')
 
-        await forbiddenLink.click()
+        forbiddenLink.scrollIntoView()
+        forbiddenLink.click()
 
-        await expect($('#linkResponse')).toHaveTextContaining('Link has responded with staus 403 and status text Forbidden')
+        linkResponse.scrollIntoView()
+        await expect(linkResponse.getText()).toHaveTextContaining('Link has responded with staus 403 and status text Forbidden')
+    })
+
+    it('Element with patial text(<element>*={text}) example', async () => {
+
+        //To find a anchor element whose visible text partially matches your search value, query it by using *= in front of the query string (e.g. *=driver).
+        const movedLink = await $('a*=Mov')
+        const linkResponse = await $('#linkResponse')
+
+        movedLink.scrollIntoView()
+        movedLink.click()
+        
+        linkResponse.scrollIntoView()
+        await expect(linkResponse.getText()).toHaveTextContaining('Link has responded with staus 301 and status text Moved Permanently')
     })
 
     it('Element with patial text(<element>*={text}) example', async () => {
 
         //To find a anchor element whose visible text partially matches your search value, query it by using *= in front of the query string (e.g. *=driver).
 
-        await browser.url('https://demoqa.com/links')
-
         const movedLink = await $('a*=Mov')
+        const linkResponse = await $('#linkResponse')
 
-        await movedLink.click()
+        movedLink.scrollIntoView()
+        movedLink.click()
 
-        await expect($('#linkResponse')).toHaveTextContaining('Link has responded with staus 301 and status text Moved Permanently')
-    })
-
-    it('Element with patial text(<element>*={text}) example', async () => {
-
-        //To find a anchor element whose visible text partially matches your search value, query it by using *= in front of the query string (e.g. *=driver).
-
-        await browser.url('https://demoqa.com/links')
-
-        const movedLink = await $('a*=Mov')
-
-        await movedLink.click()
-
-        await expect($('#linkResponse')).toHaveTextContaining('Link has responded with staus 301 and status text Moved Permanently')
+        linkResponse.scrollIntoView()
+        await expect(linkResponse.getText()).toHaveTextContaining('Link has responded with staus 301 and status text Moved Permanently')
     })
 
     it('Element with xpath example', async () => {
 
         //To find a anchor element whose visible text partially matches your search value, query it by using *= in front of the query string (e.g. *=driver).
-
-        await browser.url('https://demoqa.com/links')
-
         const notFoundLink = await $('//*[@id="invalid-url"]')
+        const linkResponse = await $('#linkResponse')
 
-        await notFoundLink.click()
+        notFoundLink.scrollIntoView()
+        notFoundLink.click()
 
-        await expect($('#linkResponse')).toHaveTextContaining('Link has responded with staus 404 and status text Not Found')
+        linkResponse.scrollIntoView()
+        await expect(linkResponse.getText()).toHaveTextContaining('Link has responded with staus 404 and status text Not Found')
     })
 
 })
