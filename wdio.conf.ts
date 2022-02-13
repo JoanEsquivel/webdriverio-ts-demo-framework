72be7cc7a23cf47360eb0b924ad4226546cd98ac
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-02-13 13:44:44 -0800

    Resetting project state
    Configuring mac os env

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -50,40 +50,23 @@ export const config: WebdriverIO.Config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [
-    //     {
+        {
 
-    //     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-    //     // grid with only 5 firefox instances available you can make sure that not more than
-    //     // 5 instances get started at a time.
-    //     maxInstances: 1,
-    //     //
-    //     browserName: 'chrome',
-    //     // 'goog:chromeOptions': {
-    //     //     args: ['--headless']
-    //     // },
-    //     acceptInsecureCerts: true
-    //     // If outputDir is provided WebdriverIO can capture driver session logs
-    //     // it is possible to configure which logTypes to include/exclude.
-    //     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-    //     // excludeDriverLogs: ['bugreport', 'server'],
-    // },
-    // {
-
-    //     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-    //     // grid with only 5 firefox instances available you can make sure that not more than
-    //     // 5 instances get started at a time.
-    //     maxInstances: 1,
-    //     //
-    //     browserName: 'firefox',
-    //     // 'moz:firefoxOptions': {
-    //     //     args: ['-headless']
-    //     // },
-    //     acceptInsecureCerts: true
-    //     // If outputDir is provided WebdriverIO can capture driver session logs
-    //     // it is possible to configure which logTypes to include/exclude.
-    //     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-    //     // excludeDriverLogs: ['bugreport', 'server'],
-    // },
+        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+        // grid with only 5 firefox instances available you can make sure that not more than
+        // 5 instances get started at a time.
+        maxInstances: 1,
+        //
+        browserName: 'chrome',
+        // 'goog:chromeOptions': {
+        //     args: ['--headless']
+        // },
+        acceptInsecureCerts: true
+        // If outputDir is provided WebdriverIO can capture driver session logs
+        // it is possible to configure which logTypes to include/exclude.
+        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+        // excludeDriverLogs: ['bugreport', 'server'],
+    },
     {
 
         // maxInstances can get overwritten per capability. So if you have an in-house Selenium
@@ -91,7 +74,7 @@ export const config: WebdriverIO.Config = {
         // 5 instances get started at a time.
         maxInstances: 1,
         //
-        browserName: 'MicrosoftEdge',
+        browserName: 'firefox',
         // 'moz:firefoxOptions': {
         //     args: ['-headless']
         // },
@@ -100,7 +83,24 @@ export const config: WebdriverIO.Config = {
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
-    }
+    },
+    // {
+
+    //     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+    //     // grid with only 5 firefox instances available you can make sure that not more than
+    //     // 5 instances get started at a time.
+    //     maxInstances: 1,
+    //     //
+    //     browserName: 'MicrosoftEdge',
+    //     // 'moz:firefoxOptions': {
+    //     //     args: ['-headless']
+    //     // },
+    //     acceptInsecureCerts: true
+    //     // If outputDir is provided WebdriverIO can capture driver session logs
+    //     // it is possible to configure which logTypes to include/exclude.
+    //     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+    //     // excludeDriverLogs: ['bugreport', 'server'],
+    // }
 ],
     //
     // ===================
