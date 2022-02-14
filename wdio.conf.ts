b15c2cee696d7730734b3988bad2c6f6c7e61dda
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-02-13 18:36:09 -0800

    Allure Reporter Included

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -171,7 +171,11 @@ export const config: WebdriverIO.Config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
+    reporters: [['allure', {
+        outputDir: 'allure-results',
+        disableWebdriverStepsReporting: true,
+        disableWebdriverScreenshotsReporting: true,
+    }]],
 
 
 
