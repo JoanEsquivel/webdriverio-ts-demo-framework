0ad61c90cdd89eef39123f6d5714adb717f5d4d4
Joan Esquivel <48256183+JoanEsquivel@users.noreply.github.com>
2022-02-27 19:48:25 -0800

    Adding video recording skill to the framework
    Using Selenium Grid 4

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -153,8 +153,8 @@ export const config: WebdriverIO.Config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
+    // services: ['docker'],
     services: ['docker'],
-
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
     // see also: https://webdriver.io/docs/frameworks
